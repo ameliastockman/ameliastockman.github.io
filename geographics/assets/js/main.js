a4c087b5af949f3e114fcfb592ef2c1bc96227d6
ameliastockman <85329549+ameliastockman@users.noreply.github.com>
2021-06-10 00:54:37 +1000

    whole site update

--- geographics/assets/js/main.js
+++ geographics/assets/js/main.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
     console.log("Hello world");
-  
-    $(".imageEarth, .imageMoon, .imageAstro, .imageLunarVehicle, .imageApolloBig, .imageApolloCircle, .imageMini, .imageRecordOne, .imageRecordThree, .imageRecordFour, .imageRecordFive, .imageRecordSix, .imageRecordSeven, .imageIssTrans, .imageIssLong, .imageIssCircle, .imageIssTriangle, .imageGnssCircle, .imageGnssSquare1, .imageGnssSquare2, .imageGnssIphone, .imageObBlue, .imageObYellow, .imageObRed, .imageEarthEclipse, .imageObCircle, .imageObBig").draggable();
+    $(".imageMoon, .imageAstro, .imageLunarVehicle, .imageApolloBig, .imageApolloCircle, .imageMini, .imageRecordOne, .imageRecordThree, .imageRecordFour, .imageRecordFive, .imageRecordSix, .imageRecordSeven, .imageIssTrans, .imageIssLong, .imageIssCircle, .imageIssTriangle, .imageGnssCircle, .imageGnssSquare1, .imageGnssSquare2, .imageGnssIphone, .imageObBlue, .imageObYellow, .imageObRed, .imageEarthEclipse, .imageObCircle, .imageObBig, .imageWindowOb1, .imageWindowOb2").draggable();
   });
 
-  function pickimg(){
-    var imagenumber = 5 ;
-    var randomnumber = Math.random() ;
-    var rand1 = Math.round( (imagenumber-1) * randomnumber) + 1;
-    images = new Array
-    images[1] = "assets/images/observation/ob-arabian-sea.jpg"
-    images[2] = "assets/images/observation/ob-surigae.jpg"
-    images[3] = "assets/images/observation/ob-sydney-flooding.jpg"
-    images[4] = "assets/images/observation/ob-lituya-bay.jpg"
-    images[5] = "assets/images/observation/ob-pacaya-eruption.jpg"
-    var image = images[rand1]
-    document.imageObSev.src = image
-    };
-    
+
+  function ImageCollection(images) {
+    this.images = images;
+    this.i = 0;
+    this.next = function(img) {
+    this.i++;
+    if (this.i == images.length)
+        this.i = 0;
+        img.src = images[this.i];
+    } 
+}
+
+  var ic1 = new ImageCollection([
+    'assets/images/observation/ob-guatemala.jpg',
+    'assets/images/observation/ob-ice.jpg',
+    'assets/images/observation/ob-australia.jpg',
+    'assets/images/observation/ob-sverdrup.jpg'
+  ]);
+
+  var ic2 = new ImageCollection([
+    'assets/images/observation/ob-surigae.jpg',
+    'assets/images/observation/ob-southcarolina.jpg',
+    'assets/images/observation/ob-laura.jpg',
+    'assets/images/observation/ob-pacaya-eruption.jpg'
+  ]);
